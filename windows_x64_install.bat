f5f8e85f023c6876aa315d8ea32e89d9f96e43d2
necromorph98 <abhinavgupta1113@gmail.com>
2023-10-10 14:43:42 +0530

    Added check for python in windows install script

--- windows_x64_install.bat
+++ windows_x64_install.bat
@@ -1,6 +1,13 @@
 @echo off
 setlocal enabledelayedexpansion
 
+rem Check if Python is installed
+python --version >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Python is not installed. Please install Python and rerun this script.
+    exit /b 1
+)
+
 rem Check if pip is installed
 pip --version >nul 2>&1
 if %errorlevel% neq 0 (
