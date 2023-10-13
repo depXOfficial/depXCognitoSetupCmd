589ca463e13ba5981214290cf126693a448ebe5a
necromorph98 <abhinavgupta1113@gmail.com>
2023-10-13 16:11:51 +0530

    Changed how pyinstaller compiles the files

--- lib/vars/vars.py
+++ lib/vars/vars.py
@@ -7,8 +7,8 @@ AWS_AUTH_URL     = f'https://api.{SERVER_NAME}/aws'
 COGNITO_CALLBACK = f'{AWS_AUTH_URL}/cognito-credentials/callback'
 
 if getattr(sys, 'frozen', False):
-    USER_DATA_DIR = os.path.join(os.path.dirname(os.path.dirname(sys.executable)), 'user_data')
-    USER_EC2_DIR = os.path.join(os.path.dirname(os.path.dirname(sys.executable)), 'ec2_key')
+    USER_DATA_DIR = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(sys.executable))), 'user_data')
+    USER_EC2_DIR = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(sys.executable))), 'ec2_key')
 elif __file__:
     USER_DATA_DIR = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'user_data')
     USER_EC2_DIR = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'ec2_key')
